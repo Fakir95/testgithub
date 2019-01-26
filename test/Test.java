47f12a0a27d04d1f740257b9dc54bcbef84b785e
Fakir95 <46577053+Fakir95@users.noreply.github.com>
2019-01-26 13:29:56 +0600

    Create Test.java

---
+++ test/Test.java
@@ -0,0 +1,27 @@
+package q;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KnnTest1 {
+
+	@SuppressWarnings("deprecation")
+	@Before 
+	public void initialize() {
+	       empty= new ArrayList();
+	@Test
+	public void testaddSize() {
+		Knn list = new Knn ();
+		list.add (list, 42); 
+		list.add (list, -3); 
+		list.add (list, 17); 
+		list.add (list, 99); 
+
+		assertEquals (4, list.size ()); 
+		assertEquals (17, list.get (2)); 
+		assertTrue (list.contains (-3, 0)); 
+		assertFalse (list.isEmpty ());
+	}
+
+}
